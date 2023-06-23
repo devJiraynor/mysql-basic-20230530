3fb868c2b947b564b3ecf408fbe7e7f6d1561412
devJiraynor <jiraynor@naver.com>
2023-06-23 18:19:41 +0900

    2023.06.23. 18:20

--- SQL_DDL.sql
+++ SQL_DDL.sql
@@ -22,6 +22,40 @@ CREATE TABLE sample_table (
     column8 DATETIME		# DATETIME : 날짜 및 시간
 );
 
+# ALTER
+# 데이터베이스, 테이블, 인덱스, 뷰 등을 수정할 때 사용하는 명령어
+
+# 테이블 수정 - 컬럼 추가
+# ALTER TABLE 테이블명 ADD 컬럼명 데이터타입;
+ALTER TABLE sample_table ADD column9 INT;
+
+# 테이블 수정 - 컬럼 타입 수정
+# ALTER TABLE 테이블명 MODIFY COLUMN 변경할컬럼명 변경될데이터타입;
+ALTER TABLE sample_table MODIFY COLUMN column9 DOUBLE;
+
+# 테이블 수정 - 컬럼 삭제
+# ALTER TABLE 테이블명 DROP COLUMN 삭제할컬럼명;
+ALTER TABLE sample_table DROP COLUMN column9;
+
+# DROP
+# 데이터베이스, 테이블, 인덱스, 뷰 등을 삭제할 때 사용하는 명령어
+
+# 테이블 삭제
+# DROP TABLE 테이블명;
+DROP TABLE sample_table;
+
+# DROP TABLE, TRUNCATE TABLE, DELETE TABLE
+# 테이블과 관련된 삭제 명령어
+# DROP (DDL) : 정의와 관련된 작업 -> 테이블의 정의 자체 삭제
+# TRUNCATE (DML) : 데이터와 관련된 작업 -> 모든 데이터 삭제 (테이블을 초기화 - 인덱스도 초기화)
+# DELETE (DML) : 데이터와 관련된 작업 -> 모든 데이터 삭제 (데이터만 삭제 - 인덱스 등은 삭제 안함)
+
+# 데이터베이스 삭제
+# DROP DATABASE 데이터베이스명;
+DROP DATABASE sample_database;
+
+
+
 
 
 
