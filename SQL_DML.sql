679ef1ee37bbc953d7efa989062a5dbe380b3f5c
devJiraynor <jiraynor@naver.com>
2023-06-26 18:18:49 +0900

    2023.06.26. 18:20

--- SQL_DML.sql
+++ SQL_DML.sql
@@ -81,5 +81,97 @@ TRUNCATE TABLE sample_table;
 # IN() : 인자로 전달된 리스트의 값이 존재하면 true, 아니면 false
 # NOT IN() : 인자로 전달된 리스트의 값이 존재하지 않으면 true, 존재하면 false
 
+SELECT * FROM namgu;
+UPDATE namgu SET 변경사유발생일자 = null WHERE 변경사유발생일자 = '';
+UPDATE namgu SET `면적_부피_길이(제곱미터)` = null WHERE `면적_부피_길이(제곱미터)` = '' OR `면적_부피_길이(제곱미터)` = '㎡';
+UPDATE namgu SET 위치_소재지 = '변경 소재지' WHERE 해체제거업자사업체명 = '우성건설' AND 순번 = 3;
+
+# 해체제거업자사업체명이 '우성건설'인 데이터의 '건물명_설비명', '위치_소재지'을 검색
+SELECT 건물명_설비명, 위치_소재지 FROM namgu WHERE 해체제거업자사업체명 = '우성건설';
+
+# 해체제거업자사업체명이 '우성건설'이 아닌 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 해체제거업자사업체명 != '우성건설';
+SELECT * FROM namgu WHERE 해체제거업자사업체명 <> '우성건설';
+
+# 순번이 5보다 작은 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 순번 <= 5;
+
+# 변경사유발생일자와 면적_부피_길이(제곱미터)가 모두 null인 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE `변경사유발생일자` <=> `면적_부피_길이(제곱미터)`;
+
+# 변경사유발생일자의 데이터가 아직 지정되어있지 않은 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 변경사유발생일자 IS NULL;
+
+# 변경사유발생일자의 데이터가 지정되어있는 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 변경사유발생일자 IS NOT NULL;
+
+# 순번이 2 ~ 5 사이인 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 순번 BETWEEN 2 AND 5;
+
+# 해체시작년월일이 2022-05-01 ~ 2022-06-30 사이인 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 해체시작년월일 BETWEEN '2022-05-01' AND '2022-06-30';
+
+# 해체시작년월일이 2022-05-01 ~ 2022-06-30 사이가 아닌 데이터의 모든 컬럼을 검색
+SELECT * FROM namgu WHERE 해체시작년월일 NOT BETWEEN '2022-05-01' AND '2022-06-30';
+
+# 논리연산자
+# AND, && : 좌항과 우항이 모두 true일 때 true를 반환, 아니면 false를 반환
+# OR, || : 좌항과 우항 중 하나로고 true일 때 true를 반환, 아니면 false를 반환
+# XOR : 좌항과 우항이 서로 다르면 true를 반환, 같으면 false
+# NOT, ! : 우항이 true면 false를 반환, true면 false를 반환
+
+# 해체제거업자사업체명이 '성수개발'이거나 '우성건설'이거나 '고은산업'인 데이터의 모든 컬럼을 검색
+SELECT * 
+FROM namgu 
+WHERE 해체제거업자사업체명 = '성수개발' 
+OR 해체제거업자사업체명 = '우성건설'
+OR 해체제거업자사업체명 = '고은산업';
+
+SELECT * FROM namgu WHERE 해체제거업자사업체명 IN('성수개발', '우성건설', '고은산업');
+SELECT * FROM namgu WHERE 해체제거업자사업체명 NOT IN('성수개발', '우성건설', '고은산업');
+
+# DISTINCT : 컬럼 검색 결과의 조합의 중복을 제거
+SELECT DISTINCT 해체제거업자사업체명 FROM namgu;
+SELECT DISTINCT 건물명_설비명, 위치_소재지 FROM namgu WHERE 해체제거업자사업체명 = '우성건설';
+
+# 패턴매칭 LIKE
+# 특정 패턴을 포함하는 데이터만 검색하기 위해 사용하는 연산자
+
+# 와일드카드
+# % : 0개 이상의 문자
+# _ : 1개의 문자
+
+# '(%,_)특정패턴문자(%,_)'
+# 1. '%용호%' : 앞뒤 어떠한 문자가 오든 '용호'라는 단어가 포함되어 있으면 true
+# 2. '%용호' : 앞 어떠한 문자가 오든 '용호'라는 단어로 끝나면 true
+# 3. '용호%' : 뒤 어떠한 문자가 오든 '용호'라는 단어로 시작하면 true
+# 4. '_용호_': 앞뒤에 한 문자가 오고 '용호'라는 단어를 포함하고 길이가 4면 true
+# 5. '_용호' : 앞에 한 문자가 오고 '용호'라는 단어로 끝나고 길이가 3이면 true
+# 6. '용호_' : 뒤에 한 문자가 오고 '용호'라는 단어로 시작하고 길이가 3이면 true
+
+# 위치_소재지에 '용호로'가 포함되어 있는 데이터의 전체 컬럼을 검색
+SELECT * FROM namgu WHERE 위치_소재지 LIKE '%용호로%';
+
+# 해체제거사업체명이 '(주)'로 시작하는 데이터의 전체 컬럼을 검색
+SELECT * FROM namgu WHERE 해체제거업자사업체명 LIKE '(주)%';
+
+SELECT * FROM namgu WHERE 해체제거업자사업체명 LIKE '%주%';
+SELECT * FROM namgu WHERE 해체제거업자사업체명 LIKE '_주%';
+
+
+SELECT * FROM namgu WHERE 해체시작년월일 LIKE '_____06___';
+
+# ORDER BY 문
+# 특정 컬럼을 기준으로 정렬 된 결과를 보고자 할 때 사용
+# ASC : 오름차순 정렬
+# DESC : 내림차순 정렬
+
+# ORDER BY column1 (ASC / DESC), ...
+# ASC는 생략 가능하고 만약 여러 개의 컬럼을 SORT문에 지정했다면 먼저 작성한 컬럼 순으로 정렬
+SELECT * FROM namgu ORDER BY 해체시작년월일;
+SELECT * FROM namgu ORDER BY 해체시작년월일 DESC;
+
+SELECT 건물명_설비명, 순번 FROM namgu ORDER BY 건물명_설비명, 순번 DESC;
+SELECT 건물명_설비명, 순번 FROM namgu ORDER BY 순번 DESC, 건물명_설비명;
 
 
