77444701bd328fe932a94a547d67948a8207ed42
devJiraynor <jiraynor@naver.com>
2023-06-29 17:26:41 +0900

    2023.06.29. 17:20

--- SQL_JOIN_SUB.sql
+++ SQL_JOIN_SUB.sql
@@ -96,3 +96,84 @@ WHERE writer_email IN (
 	SELECT email FROM user WHERE nickname = 'nickname1'
 );
 
+SELECT board.* FROM board
+LEFT OUTER JOIN user
+ON board.writer_email = user.email
+WHERE nickname = 'nickname1';
+
+# SELECT 절에서 서브쿼리 사용
+
+SELECT 
+	board_number, 
+	(SELECT nickname FROM user WHERE email = 'email@email.com') AS nickname
+FROM board
+WHERE writer_email = 'email@email.com';
+
+# FROM 절에서 서브쿼리 사용
+
+SELECT * FROM (SELECT * FROM user WHERE address = '부산광역시 부산진구') AS T1;
+
+SELECT * FROM board
+INNER JOIN (SELECT * FROM user WHERE address = '부산광역시 부산진구') AS T1
+ON board.writer_email = T1.email;
+
+# VIEW
+# 복잡한 SELECT 문을 특정한 이름으로 지정해놓은 스키마 (READ-ONLY)
+# 읽기 전용의 '가상' 테이블
+
+# CREATE VIEW view_name AS (SELECT ~);
+CREATE VIEW latest_board AS 
+(
+	SELECT 
+		b.board_number AS board_number, b.title, b.content, b.image, b.view_count, b.write_datetime,
+		u.profile_image, u.nickname
+	FROM board AS b
+	INNER JOIN user AS u
+	ON b.writer_email = u.email
+	ORDER BY write_datetime DESC
+);
+
+SELECT * FROM latest_board;
+
+# 집계 함수
+# 여러 행의 데이터를 그룹화하여 하나의 결과를 반환하는 함수 (일반적으로 수를 연산)
+
+# count()
+# 행의 갯수 반환
+SELECT count(*) FROM user;
+# 특정 조건으로 검색
+SELECT count(*) FROM user WHERE email = 'email@email.com';
+# 특정 컬럼으로 검색
+SELECT count(profile_image), count(email) FROM user;
+
+# MAX, MIN
+# 특정 컬럼의 최대, 최소 값을 구함
+SELECT MAX(view_count), MIN(view_count) FROM board;
+
+USE sample_database;
+
+# GROUP BY
+# SELECT 문에서 반환되는 행들을 하나이 이상의 컬럼을 기준으로 그룹화 할때 사용
+SELECT * FROM namgu;
+
+SELECT 해체제거업자사업체명, count(해체제거업자사업체명) FROM namgu
+GROUP BY 해체제거업자사업체명;
+
+SELECT 해체제거업자사업체명, 공사현장명, count(해체제거업자사업체명) FROM namgu
+GROUP BY 해체제거업자사업체명, 공사현장명;
+
+# HAVING
+# GROUP BY 결과에 조건을 걸어서 출력(필터링)을 하기위한 구문
+# WHERE 절과 비슷한 사용 방법을 가지고 있음
+# WHERE 절과의 차이는 WHERE 절은 실제 데이터(테이블 자체)에서 조건을 걸고
+# HAVING 절은 SELECT의 결과에서 조건에 해당하는 데이터를 출력
+
+SELECT 해체제거업자사업체명, count(해체제거업자사업체명) AS 업체수 FROM namgu
+WHERE 해체제거업자사업체명 != '명작건설주식회사'
+GROUP BY 해체제거업자사업체명
+HAVING 업체수 <= 3;
+
+
+
+
+
